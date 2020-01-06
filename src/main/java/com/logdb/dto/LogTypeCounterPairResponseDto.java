e06a5cd9813141f8f4146368dc451db99c3a0a29
Grigoris Dimopoulos <grigoriosdimopoulos@camelotls.com>
2020-01-06 13:02:18 +0200

    Added some dtos

---
+++ src/main/java/com/logdb/dto/LogTypeCounterPairResponseDto.java
@@ -0,0 +1,22 @@
+package com.logdb.dto;
+
+public class LogTypeCounterPairResponseDto {
+    private String type;
+    private Integer numberOfLogs;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getNumberOfLogs() {
+        return numberOfLogs;
+    }
+
+    public void setNumberOfLogs(Integer numberOfLogs) {
+        this.numberOfLogs = numberOfLogs;
+    }
+}
